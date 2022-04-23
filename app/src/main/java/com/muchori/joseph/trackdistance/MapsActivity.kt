18323ef5f16a963cb677cb48e04fae6c20e3d501
Muchori Joseph <muchorijoseph97@gmail.com>
2022-04-24 00:12:08 +0300

    Camera twisting & Styling of Map

--- app/src/main/java/com/muchori/joseph/trackdistance/MapsActivity.kt
+++ app/src/main/java/com/muchori/joseph/trackdistance/MapsActivity.kt
@@ -2,18 +2,23 @@ package com.muchori.joseph.trackdistance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import com.muchori.joseph.trackdistance.databinding.ActivityMapsBinding
+import java.lang.Exception
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-  private lateinit var mMap: GoogleMap
+  private lateinit var map: GoogleMap
   private lateinit var binding: ActivityMapsBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,21 +33,60 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     mapFragment.getMapAsync(this)
   }
 
-  /**
-   * Manipulates the map once available.
-   * This callback is triggered when the map is ready to be used.
-   * This is where we can add markers or lines, add listeners or move the camera. In this case,
-   * we just add a marker near Sydney, Australia.
-   * If Google Play services is not installed on the device, the user will be prompted to install
-   * it inside the SupportMapFragment. This method will only be triggered once the user has
-   * installed Google Play services and returned to the app.
-   */
-  override fun onMapReady(googleMap: GoogleMap) {
-    mMap = googleMap
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.maps_menu_types, menu)
 
-    // Add a marker in Sydney and move the camera
-    val sydney = LatLng(-34.0, 151.0)
-    mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when(item.itemId) {
+      R.id.normal_map -> {
+        map.mapType = GoogleMap.MAP_TYPE_NORMAL
+      }
+      R.id.hybrid_map -> {
+        map.mapType = GoogleMap.MAP_TYPE_HYBRID
+      }
+      R.id.satellite_map -> {
+        map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+      }
+      R.id.terrain_map -> {
+        map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+      }
+      R.id.none_map -> {
+        map.mapType = GoogleMap.MAP_TYPE_NONE
+      }
+    }
+
+    return true
+  }
+
+  override fun onMapReady(googleMap: GoogleMap) {
+    map = googleMap
+
+    // Add a marker in Meru and move the camera to 10f
+    val meru = LatLng(0.07923793565480615, 37.643490681407854)
+    map.addMarker(MarkerOptions().position(meru).title("Meru"))
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(meru, 10f))
+    map.uiSettings.apply {
+      isZoomControlsEnabled = true
+    }
+
+   setMapStyle(map)
+  }
+
+  private fun setMapStyle(googleMap: GoogleMap) {
+    try {
+      val success = googleMap.setMapStyle(
+        MapStyleOptions.loadRawResourceStyle(
+          this, R.raw.style
+        )
+      )
+      if(!success) {
+        Log.d("Maps", "Failed to load Style")
+      }
+    } catch (e: Exception) {
+      Log.d("MAps", e.toString())
+    }
   }
 }
